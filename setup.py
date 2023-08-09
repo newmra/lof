b4c889622a6c23e28fc8ca62abd791f7d080e313
RalNeu <neumann.ralph@gmx.de>
2023-08-09 16:52:02 +0200

    Setup project

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+lof: Live Object Framework
+     Simplistic event framework which enables unrelated objects to share information, alter each other states and
+     provides the possibility to undo made changes.
+
+Author: Ralph Neumann
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, see <https://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lof',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[]
+)
