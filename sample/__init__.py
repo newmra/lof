fc0f078f3f3a3148993a096917569e820635c059
RalNeu <neumann.ralph@gmx.de>
2024-05-01 11:24:16 +0200

    Fix all issues and implement unittests

--- sample/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    Copyright (c) 2023 - All rights reserved
-    Unauthorized copying of this file, via any medium is strictly prohibited
-
-    File:       __init__.py
-    Created on: 10. Aug.. 2023
-    Author:     Ralph Neumann
-"""
--- xnodes/__init__.py
+++ xnodes/__init__.py
@@ -1,14 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 """
 xnodes: Exchange nodes framework
         Simplistic event framework which enables unrelated nodes to exchange information, alter each other states and
         provides the possibility to undo made changes.
 
-Author: Ralph Neumann
+Author: Ralph Neumann (@newmra)
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, see <https://www.gnu.org/licenses/>.
+This framework is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, see <https://www.gnu.org/licenses>.
 """
+
+from .x_core import X_CORE_NODE_IDENTIFIER, X_CORE_START, X_UNDO_EVENT, X_REDO_EVENT, X_MAP_UNDO_REDO_COUNTERS, \
+    X_CLEAR_UNDO_REDO_EVENTS, register_event, register_node, unregister_node, start, publish, broadcast, add_undo_events
+from .x_event_handler import x_event_handler
+from .x_node import XNode
+
+__all__ = [
+    "register_event",
+    "register_node",
+    "unregister_node",
+    "start",
+    "publish",
+    "broadcast",
+    "add_undo_events",
+    "X_CORE_NODE_IDENTIFIER",
+    "X_CORE_START",
+    "X_UNDO_EVENT",
+    "X_REDO_EVENT",
+    "X_MAP_UNDO_REDO_COUNTERS",
+    "X_CLEAR_UNDO_REDO_EVENTS",
+    "x_event_handler",
+    "XNode"
+]
